29755ffd0ba53117211fa038c961e62b99d59f78
frking69 <121990793+frking69@users.noreply.github.com>
2024-05-21 20:57:51 +0700

    Create Stasiun.java

---
+++ Stasiun.java
@@ -0,0 +1,11 @@
+public class Stasiun {
+	String namaStasiun;
+    String kodeStasiun;
+    String lokasi;
+
+    public Stasiun(String namaStasiun, String kodeStasiun, String lokasi) {
+        this.namaStasiun = namaStasiun;
+        this.kodeStasiun = kodeStasiun;
+        this.lokasi = lokasi;
+    }
+}
